f41f4be007a8f3ad7ce6356cdb0e498e357ba51d
shawnbr <srb286@cornell.edu>
2016-09-12 12:11:04 -0400

    Corrections up to tutorial 4

--- snippets/models.py
+++ snippets/models.py
@@ -10,9 +10,6 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
-owner = models.ForeignKey('auth.User', related_name='snippets')
-highlighted = models.TextField()
-
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -22,6 +19,7 @@ class Snippet(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
     owner = models.ForeignKey('auth.User', related_name='snippets')
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ('created',)
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,9 +2,8 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-owner = serializers.ReadOnlyField(source='owner.username')
-
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
         fields = ('owner', 'id', 'title', 'code', 'linenos', 'language', 'style')
